e2337c6ee8654e89535c9faa168e8ed82b6eef53
bricsi0000000000000 <bolya.richard@gmail.com>
2021-02-28 16:49:27 +0100

    Add Person form

--- ContractStore/ContractStore/Models/People/Person.cs
+++ ContractStore/ContractStore/Models/People/Person.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ContractStore.Models
+namespace ContractStore.Models.People
 {
     public class Person
     {
         public int ID { get; set; }
+
+        [DisplayName("First name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "required")]
         public string FirstName { get; set; }
+
+        [DisplayName("Last name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "required")]
         public string LastName { get; set; }
+
         public string BirthName { get; set; }
         public string MotherName { get; set; }
         public string Nationality { get; set; }
         public string BirthPlace { get; set; }
+
+        [DisplayName("Birth date")]
+        [Required(ErrorMessage = "Birth date is required")]
         public DateTime BirthDate { get; set; }
         public enum GenderType { female, male }
         public GenderType Gender { get; set; }
---
+++ ContractStore/ContractStore/Models/People/PersonManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ContractStore.Models.People
+{
+    public static class PersonManager
+    {
+        public static List<Person> People { get; set; } = new List<Person>();
+
+        //TODO add, delete, get person
+    }
+}
