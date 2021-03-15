5492c7208b4ca41b145c25d8c0aedf280ee578f9
Patorik <p4t0r1k@yahoo.com>
2021-03-15 16:00:32 +0100

    Vehicle and VehicleManager basic classes finished

--- ContractStore/ContractStore/Models/Vehicle/Vehicle.cs
+++ ContractStore/ContractStore/Models/Vehicle/Vehicle.cs
@@ -12,23 +12,44 @@ namespace ContractStore.Models.Vehicle
     {
         public int ID { get; set; }
 
+        [DisplayName("Jármű típusa")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Válasszon típust!")]
         public string VehicleType { get; set; }
 
+
+        [DisplayName("Gyártó")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Válasszon gyártót!")]
         public string Manufacturer { get; set; }
 
+
+        [DisplayName("Gyártmány")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Válasszon gyártmányt!")]
         public string ManuType { get; set; }
 
         [DisplayName("Gyártás éve")]
-        [Required(ErrorMessage = "Válassz gyártási évet!")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Válasszon gyártási évet!")]
         public int ProductYear { get; set; }
+
+        [DisplayName("Jármű Kategóriája")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Válasszon kategóriát!")]
         public string Category { get; set; }
+
+        [DisplayName("Érvényesség ideje")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Válassza ki az érvényesség idejét!")]
         public DateTime ValidityBegin { get; set; }
+        [DisplayName("Jármű Kategóriája")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Válassza ki a jármű tömegét!")]
         public int Mass { get; set; }
+        [DisplayName("Szállítható személyek száma")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Válassza ki a szállítható személyek számát!")]
         public int TransportablePeople { get; set; }
+        [DisplayName("Ülések száma")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Válassza ki az ülések számát!")]
         public int NumberOfSeats { get; set; }
+        [DisplayName("Állóhelyek száma")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Válassza ki az állóhelyek számát!")]
         public int NumberOfStandingPlaces { get; set; }
         public double RateOfPerformance { get; set; }
-
         public string TractionData { get; set; }
         public DateTime TechnicalValidity{ get; set; }
         public DateTime RegisterDate{ get; set; }
