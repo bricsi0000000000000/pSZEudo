0eb32382c7053eec2e138e661de302daa3300bdd
bricsi0000000000000 <bolya.richard@gmail.com>
2021-03-20 14:21:23 +0100

    Add more data validation

--- ContractStore/ContractStore/Models/People/Person.cs
+++ ContractStore/ContractStore/Models/People/Person.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ContractStore.Models.People
 {
@@ -13,7 +10,7 @@ namespace ContractStore.Models.People
 
         [DisplayName("Viselt név")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "A mező kitöltése kötelező!")]
-        public string WornName { get; set; }
+        public string WornName { get; set; } = "";
 
         [DisplayName("Név előtag")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "A mező kitöltése kötelező!")]
@@ -29,7 +26,7 @@ namespace ContractStore.Models.People
 
         [DisplayName("Születési név")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "A mező kitöltése kötelező!")]
-        public string BirthName { get; set; }
+        public string BirthName { get; set; } = "";
 
         [DisplayName("Anyja neve")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "A mező kitöltése kötelező!")]
@@ -53,14 +50,17 @@ namespace ContractStore.Models.People
 
         [DisplayName("Telefonszám")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "A mező kitöltése kötelező!")]
+        [RegularExpression(@"\+[0-9]{11}$", ErrorMessage = "A telefonszám nem megfelelő formátumú. Megfelelő formátum: +36201234567")]
         public string PhoneNumber { get; set; }
 
         [DisplayName("E-mail cím")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "A mező kitöltése kötelező!")]
+        [RegularExpression(@"[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+", ErrorMessage = "Az email cim nem megfelelő formátumú.")]
         public string Email { get; set; }
 
         [DisplayName("Irányítószám")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "A mező kitöltése kötelező!")]
+        [RegularExpression(@"[0-9]{4}", ErrorMessage = "Az irányitószám nem megfelelő formátumú.")]
         public int PostCode { get; set; }
 
         [DisplayName("Város")]
@@ -73,6 +73,7 @@ namespace ContractStore.Models.People
 
         [DisplayName("Házszám")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "A mező kitöltése kötelező!")]
+        [RegularExpression(@"[0-9]", ErrorMessage = "A házszám nem megfelelő formátumú.")]
         public int HouseNumber { get; set; }
 
         [DisplayName("Személyi igazolvány száma")]
@@ -81,7 +82,7 @@ namespace ContractStore.Models.People
 
         [DisplayName("Adószám")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "A mező kitöltése kötelező!")]
-
+        [RegularExpression(@"[0-9]", ErrorMessage = "Az adószám nem megfelelő formátumú.")]
         public int TaxNumber { get; set; }
     }
 }
