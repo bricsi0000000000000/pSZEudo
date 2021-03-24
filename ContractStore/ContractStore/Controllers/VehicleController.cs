7081213ac3a776aec26c2b6a79b2e539b41d1389
kokohun <natzkornel@gmail.com>
2021-03-24 14:14:12 +0100

    Fixed DateTime Bug

--- ContractStore/ContractStore/Controllers/VehicleController.cs
+++ ContractStore/ContractStore/Controllers/VehicleController.cs
@@ -20,7 +20,27 @@ namespace ContractStore.Controllers
         [HttpPost]
         public IActionResult AddVehicle(Vehicle vehicle, IFormCollection collection)
         {
-            VehicleManager.VehicleList.Add(vehicle);
+            var incomingValidityBegin = new DateTime(int.Parse(collection["ValidityBegin.Year"]),
+                                                 int.Parse(collection["ValidityBegin.Month"]),
+                                                 int.Parse(collection["ValidityBegin.Day"]));
+            vehicle.ValidityBegin = incomingValidityBegin;
+
+            var incomingTechnicalValidity = new DateTime(int.Parse(collection["TechnicalValidity.Year"]),
+                                                 int.Parse(collection["TechnicalValidity.Month"]),
+                                                 int.Parse(collection["TechnicalValidity.Day"]));
+            vehicle.TechnicalValidity = incomingTechnicalValidity;
+
+            var incomingRegisterDate = new DateTime(int.Parse(collection["RegisterDate.Year"]),
+                                                 int.Parse(collection["RegisterDate.Month"]),
+                                                 int.Parse(collection["RegisterDate.Day"]));
+            vehicle.RegisterDate = incomingRegisterDate;
+
+            var incomingPlacedInTrafficDate = new DateTime(int.Parse(collection["PlacedInTrafficDate.Year"]),
+                                                 int.Parse(collection["PlacedInTrafficDate.Month"]),
+                                                 int.Parse(collection["PlacedInTrafficDate.Day"]));
+            vehicle.PlacedInTrafficDate = incomingPlacedInTrafficDate;
+
+            VehicleManager.addToList(vehicle);
 
             return View("Index", VehicleManager.VehicleList);
         }
